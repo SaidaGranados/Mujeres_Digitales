1959dbca148922476a48706d6825b3e1d4656925
SaidaGranados <adiassaro@gmail.com>
2021-08-25 18:16:31 -0500

    Archivo localStorage/index.js Local Storage parte 1 12-08-2021

---
+++ 12-08-2021/localStorage/index.js
@@ -0,0 +1,54 @@
+console.log('hola mundo')
+
+
+/********************LOCAL STORAGE********************/
+
+// Metodo setItem: Para gravar datos en el localStorage: localStorage.setItem("key", "value");
+//localStorage.setItem('nombre',"castiblanco")
+// Ejercicio 1: programa donde se ingrese info y se almacene en el local storage
+/*var guardar = function(){
+    var nombre = document.getElementById('nombre').value
+    var apellido = document.getElementById('apellido').value
+    localStorage.setItem('nombre',nombre)
+    localStorage.setItem('apellido',apellido)
+}*/
+
+// Metodo getItem: Para extraer la informacion del local storage. var lastname = localStorage.getItem("key");
+/*var cargar = function(){
+    document.getElementById('nombre').value = localStorage.getItem('nombre')
+    document.getElementById('apellido').value = localStorage.getItem('apellido')
+
+}*/
+
+// Metodo removeItem: 
+/*var limpiar = function(){
+    document.getElementById('nombre').value = localStorage.removeItem('nombre')
+    
+}*/
+
+// Ejercicio de alamacenar  informacion usando arrays . Convertir de un tipo array a String
+// Utilizamos JSON:stringify() para convertir un objeto js {},{},{} a un formato JSON [{},{},{}]
+var misdatos = [];
+var guardar = function(){
+    var nombre = document.getElementById('nombre').value
+    var apellido = document.getElementById('apellido').value
+
+    misdatos.push({nombre:nombre, apellido:apellido})
+    var x = JSON.stringify(misdatos)
+    console.log(x)
+    localStorage.setItem('datos',x)
+    
+}
+
+// Metodo JSON: JSON.parse() para convertir de un formato JSON [{},{},{}] lo que esta en local storage a un objeto js {},{},{}
+misdatos = JSON.parse(localStorage.getItem('datos'.toString())
+
+
+
+
+
+
+
+
+
+
